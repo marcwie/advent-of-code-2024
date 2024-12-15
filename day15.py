de5f8c2fd65cea955097d91eb24c08a8e62c8bc1
Marc Wiedermann <mawiederm@gmail.com>
2024-12-15 10:35:33 +0100

    day 15 solution

--- day15.py
+++ day15.py
@@ -28,9 +28,75 @@ def load(input_file):
     return robot, walls, boxes, movements
 
 
-def solve(input_file):
+def expand_map(robot, walls, boxes):
+
+    expanded_walls = [(x, 2 * y + k) for x, y in walls for k in (0, 1)]
+    expanded_boxes = [(x, 2 * y + k) for x, y in boxes for k in (0, 1)]
+    expanded_robot = (robot[0], 2 * robot[1])
+
+    box_connection = {}
+    for i in range(len(boxes)):
+        box_connection[2 * i] = 2 * i + 1
+        box_connection[2 * i + 1] = 2 * i
+
+    return expanded_robot, expanded_walls, expanded_boxes, box_connection
+
+
+def move_horizontal(x, y, dy, boxes, walls):
+
+    box_line = [(x, y + dy)]
+
+    while (box_line[-1][0], box_line[-1][1] + dy) in boxes:
+        box_line.append((box_line[-1][0], box_line[-1][1] + dy))
+
+    if (box_line[-1][0], box_line[-1][1] + dy) not in walls:
+        y = y + dy
+        box_indices = [boxes.index(box) for box in box_line]
+        for j, (box_x, box_y) in zip(box_indices, box_line):
+            boxes[j] = (box_x, box_y + dy)
+
+    return x, y, boxes
+
+
+def move_vertical(x, y, dx, boxes, box_connection, walls):
+
+    blocking_box = boxes.index((x + dx, y))
+    box_line = set((boxes[blocking_box], boxes[box_connection[blocking_box]]))
+    n_boxes = 0
+
+    while n_boxes != len(box_line):
+        n_boxes = len(box_line)
+
+        added_boxes = set()
+        for box in box_line:
+            if (box[0] + dx, box[1]) in boxes:
+                blocking_box = boxes.index((box[0] + dx, box[1]))
+                added_boxes.add(boxes[blocking_box])
+                added_boxes.add(boxes[box_connection[blocking_box]])
+
+        box_line |= added_boxes
+
+    for box in box_line:
+        if (box[0] + dx, box[1]) in walls:
+            return x, y, boxes
+
+    x = x + dx
+    box_indices = [boxes.index(box) for box in box_line]
+    for j, (box_x, box_y) in zip(box_indices, box_line):
+        boxes[j] = (box_x + dx, box_y)
+
+    return x, y, boxes
+
+
+def solve(input_file, use_expanded_map=False):
+
     robot, walls, boxes, movements = load(input_file)
     directions = [DIRECTIONS[move] for move in movements]
+
+    if use_expanded_map:
+        robot, walls, boxes, box_connection = expand_map(robot, walls, boxes)
+    else:
+        box_connection = {i: i for i in range(len(boxes))}
 
     x, y = robot
     for dx, dy in directions:
@@ -38,17 +104,14 @@ def solve(input_file):
         if (x + dx, y + dy) not in walls and (x + dx, y + dy) not in boxes:
             x, y = x + dx, y + dy
 
-        elif (x + dx, y + dy) in boxes:
-            box_line = [(x + dx, y + dy)]
+        elif (x + dx, y + dy) in boxes and dy != 0:
+            x, y, boxes = move_horizontal(x, y, dy, boxes, walls)
 
-            while (box_line[-1][0] + dx, box_line[-1][1] + dy) in boxes:
-                box_line.append((box_line[-1][0] + dx, box_line[-1][1] + dy))
+        elif (x + dx, y + dy) in boxes and dy == 0:
+            x, y, boxes = move_vertical(x, y, dx, boxes, box_connection, walls)
 
-            if (box_line[-1][0] + dx, box_line[-1][1] + dy) not in walls:
-                x, y = x + dx, y + dy
-                box_indices = [boxes.index(box) for box in box_line]
-                for j, (box_x, box_y) in zip(box_indices, box_line):
-                    boxes[j] = (box_x + dx, box_y + dy)
+    if use_expanded_map:
+        boxes = boxes[::2]
 
     return sum([100 * x + y for x, y in boxes])
 
@@ -59,7 +122,8 @@ def main():
     parser.add_argument("input_file", type=str)
     args = parser.parse_args()
 
-    print("Part 1 solution:", solve(args.input_file))
+    print("Part 1 solution:", solve(args.input_file, False))
+    print("Part 2 solution:", solve(args.input_file, True))
 
 
 if __name__ == "__main__":
