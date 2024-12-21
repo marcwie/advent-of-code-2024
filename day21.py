f35e8aff666f186d40adf2f1efd65aba3ac16a48
Marc Wiedermann <mawiederm@gmail.com>
2024-12-21 20:01:32 +0100

    removed redundant code

--- day21.py
+++ day21.py
@@ -15,28 +15,37 @@ def load(input_file):
 
 
 def buttons_coords(pad):
-
+    """The coordinates of each button on either of the two pads."""
     combinations = product(range(len(pad)), range(len(pad[0])))
     return {pad[i][j]: (i, j) for i, j in combinations if pad[i][j] != "."}
 
 
-def manhattan_distance(start, end):
-    return abs(start[0] - end[0]) + abs(start[1] - end[1])
+def manhattan_distance(point1, point2):
+    """Manhattan distance between two points."""
+    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
 
 
 def is_grouped(sequence):
+    """
+    Determine whether direction sequences are properly grouped.
+
+    Returns True for sequences such as '>>^^' or 'vv>' and False for '<^<'.
+    """
     for i in range(1, len(sequence)):
         if sequence[i] != sequence[i - 1] and sequence[i] in sequence[:i]:
             return False
     return True
 
 
-def get_move_sequence(pad, start, end):
+def move_sequence(pad, start, end):
+
+    if start == end:
+        return "A"
 
     buttons = buttons_coords(pad)
     valid_coords = buttons.values()
-
     dist = manhattan_distance(buttons[start], buttons[end])
+
     x, y = buttons[start]
     sequence = ""
     stack = [(x, y, sequence)]
@@ -46,50 +55,42 @@ def get_move_sequence(pad, start, end):
         for dx, dy, direction in DIRECTIONS:
             if len(sequence + direction) < dist and (x + dx, y + dy) in valid_coords:
                 stack.append((x + dx, y + dy, sequence + direction))
-            elif (x + dx, y + dy) == buttons[end]:
+            elif (x + dx, y + dy) == buttons[end] and is_grouped(sequence + direction):
                 valid_sequence.append(sequence + direction + "A")
 
         x, y, sequence = stack.pop()
 
-    return [s for s in valid_sequence if is_grouped(s)]
+    if len(valid_sequence) == 2:
+        return compare(*valid_sequence)
+
+    return valid_sequence[0]
 
 
-def get_all_move_sequences(pad):
+def all_move_sequences(pad):
 
     buttons = buttons_coords(pad)
     combinations = product(buttons.keys(), buttons.keys())
-    sequences = {(p0, p1): get_move_sequence(pad, p0, p1) for p0, p1 in combinations}
+    sequences = {(p0, p1): move_sequence(pad, p0, p1) for p0, p1 in combinations}
 
     return sequences
 
 
-def get_required_moves(code):
+def required_moves(sequence):
 
-    moves = Counter(zip(code[:-1], code[1:]))
-    moves[("A", code[0])] += 1
+    moves = Counter(zip(sequence[:-1], sequence[1:]))
+    moves[("A", sequence[0])] += 1
 
     return moves
 
 
 def one_run(sequences, moves):
 
-    next_counter = Counter()
+    counter = Counter()
     for move, count in moves.items():
-        for next_move, next_count in get_required_moves(sequences[move]).items():
-            next_counter[next_move] += count * next_count
+        for next_move, next_count in required_moves(sequences[move]).items():
+            counter[next_move] += count * next_count
 
-    return next_counter
-
-
-def get_best_robot_moves(robot_sequences):
-
-    for keys, value in robot_sequences.items():
-        if keys[0] == keys[1]:
-            robot_sequences[keys] = "A"
-        elif len(value) == 2:
-            robot_sequences[keys] = compare(*value)
-        else:
-            robot_sequences[keys] = value[0]
+    return counter
 
 
 def compare(s1, s2):
@@ -97,32 +98,16 @@ def compare(s1, s2):
     return s1 if preferences.index(s1[0]) < preferences.index(s2[0]) else s2
 
 
-def get_best_keypad_moves(keypad_sequences, robot_sequences, n_robots):
-
-    for key, value in keypad_sequences.items():
-        if len(value) > 1:
-            keypad_sequences[key] = compare(*value)
-        else:
-            keypad_sequences[key] = value[0] if value else ""
-
-
 def solve(input_file, n_robots):
 
     data = load(input_file)
-
-    keypad_sequences = get_all_move_sequences(PAD)
-    robot_sequences = get_all_move_sequences(ROBOT_PAD)
-
-    get_best_robot_moves(robot_sequences)
-    get_best_keypad_moves(keypad_sequences, robot_sequences, n_robots)
+    keypad_sequences = all_move_sequences(PAD)
+    robot_sequences = all_move_sequences(ROBOT_PAD)
 
     result = 0
-
     for code in data:
-
-        moves = get_required_moves(code)
+        moves = required_moves(code)
         moves = one_run(keypad_sequences, moves)
-
         for _ in range(n_robots):
             moves = one_run(robot_sequences, moves)
 
